86e9bb21b521560ccf375df16e4b26f6d5ca220c
ywseok <ywseok@daeyang.co.kr>
2025-07-28 11:29:48 +0900

    초기 커밋.

---
+++ TestingGround/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ToolKit
+{
+    internal class Program
+    {
+        // ToolKit 기능 시험을 위한 코드입니다.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
