4db257776bbf405b3f02123c69e825797bc4d70b
CountryBird <yangwonseog0727@gmail.com>
2025-07-29 07:37:39 +0900

    ChekckKit 추가

---
+++ ToolKit/CheckKit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolKit
+{
+    public static class CheckKit
+    {
+        public static bool IsSame<T>(T? item1, T? item2) // 두 객체가 같은지 비교
+        {
+            if(item1 == null && item2 == null) return true;
+            if(item1 == null || item2 == null) return false;
+
+            return Equals(item1, item2);
+        }
+    }
+}
