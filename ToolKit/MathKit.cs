59525b0f4e82ea85f640a378bf1069167ed2b2e5
CountryBird <yangwonseog0727@gmail.com>
2025-08-15 07:56:36 +0900

    MathKit 추가

---
+++ ToolKit/MathKit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolKit
+{
+    public static class MathKit
+    {
+        public static double ClampMin(double value, double min) // 최소값 제한
+        {
+            return value < min ? min : value;
+        }
+        public static double ClampMax(double value, double max) // 최대값 제한
+        {
+            return value > max ? max : value;
+        }
+    }
+}
